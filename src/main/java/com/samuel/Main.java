48f042c541e0bb9bb244992cb670ae911e053d97
Samuel <sferrercarot@gmail.com>
2024-12-04 12:10:13 +0100

    4/12/24

--- src/main/java/com/samuel/Main.java
+++ src/main/java/com/samuel/Main.java
@@ -41,7 +41,20 @@ public class Main {
         } else {
             System.out.println("Error al cargar los datos desde el archivo JSON.");
         }
-    }
+            ListaEquipos equiposRss = cargarDatosDesdeJSON("src/main/resources/Equipos.json");
+
+            if (equiposRss != null) {
+                String rutaRSS = "src/main/resources/static/EquiposRSS.xml";
+                String titulo = "Equipos de Fútbol - RSS Feed";
+                String descripcion = "RSS con información sobre equipos y jugadores destacados.";
+                generaRSS(equiposRss, rutaRSS, titulo, descripcion);
+
+                System.out.println("Generación de RSS completada.");
+            } else {
+                System.out.println("Error al cargar los datos desde el archivo JSON.");
+            }
+        }
+
 
     public static ListaEquipos cargarDatosDesdeJSON(String path) {
         ObjectMapper objectMapper = new ObjectMapper();
@@ -56,6 +69,47 @@ public class Main {
     public static void escribirHTML(String contenido, String nombreArchivo) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(nombreArchivo))) {
             writer.write(contenido);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void generaRSS(ListaEquipos equipos, String rutaRSS, String titulo, String descripcion) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(rutaRSS))) {
+            bw.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+            bw.write("<rss version=\"2.0\">\n");
+            bw.write("<channel>\n");
+
+            bw.write("<title>" + titulo + "</title>\n");
+            bw.write("<link>http://localhost:8080</link>\n");
+            bw.write("<description>" + descripcion + "</description>\n");
+
+            for (Equipo equipo : equipos.getEquipos()) {
+                bw.write("<item>\n");
+                bw.write("<title>" + equipo.getNombre() + "</title>\n");
+                bw.write("<link>http://localhost:8080/detalles_" + equipo.getNombre().replaceAll(" ", "_") + ".html</link>\n");
+                bw.write("<description>\n");
+                bw.write("Equipo fundado en " + equipo.getFundado() + " en la ciudad de " + equipo.getCiudad() +
+                        ". Estadio: " + equipo.getEstadio() + ". Entrenador: " + equipo.getEntrenador() + ".\n");
+                bw.write("</description>\n");
+
+                for (Jugador jugador : equipo.getJugadores()) {
+                    bw.write("<item>\n");
+                    bw.write("<title>" + jugador.getNombre() + " (" + jugador.getPosicion() + ")</title>\n");
+                    bw.write("<link>" + jugador.getImagen() + "</link>\n");
+                    bw.write("<description>" + jugador.getPosicion() + ", Nacionalidad: " + jugador.getNacionalidad() +
+                            ", Edad: " + jugador.getEdad() + " años.\n</description>\n");
+                    bw.write("</item>\n");
+                }
+
+                bw.write("</item>\n");
+            }
+
+            // Cerrar canal RSS
+            bw.write("</channel>\n");
+            bw.write("</rss>\n");
+
+            System.out.println("RSS generado correctamente en " + rutaRSS);
         } catch (IOException e) {
             e.printStackTrace();
         }
